cf1722efc7cc512bb59fcfd5052ceb6847c242b3
Robert Buonpastore <robert.buonpastore@gmail.com>
2013-06-05 11:14:23 -0400

    Many changes to tic-tac-toe game
    - Added remaining winning positions
    - Enhanced visuals through show function
    - Made it into a two-player game
    - Added check for move to same position
    - Modified won function to return the player that won

--- tictactoe.py
+++ tictactoe.py
@@ -6,38 +6,65 @@ BOARD = [[' ', ' ', ' '],
 
 WINNING_POSITIONS = [[(0, 0), (0, 1), (0, 2)],
                      [(1, 0), (1, 1), (1, 2)],
-                     [(2, 0), (2, 1), (2, 2)]]
+                     [(2, 0), (2, 1), (2, 2)],
+                     [(0, 0), (1, 0), (2, 0)],
+                     [(0, 1), (1, 1), (2, 1)],
+                     [(0, 2), (1, 2), (2, 2)],
+                     [(0, 0), (1, 1), (2, 2)],
+                     [(2, 0), (1, 1), (0, 2)]]
 
 X_VALS = []
 O_VALS = []
 
-def move():
+def move(player):
     row = int(raw_input())
     col = int(raw_input())
-    X_VALS.append((row, col))
-    BOARD[row][col]= "x"
+
+    if (row, col) in X_VALS or\
+       (row, col) in O_VALS:
+        return None
+
+    if player == 1:
+        X_VALS.append((row, col))
+        BOARD[row][col] = 'x'
+    else:
+        O_VALS.append((row, col))
+        BOARD[row][col] = 'o'
+
+    return 1
 
 def show():
-    for row in BOARD:
-        print '|'.join(row)
+    print ' ' + ' | '.join(BOARD[0])
+    print '---|---|---'
+    print ' ' + ' | '.join(BOARD[1])
+    print '---|---|---'
+    print ' ' + ' | '.join(BOARD[2])
 
-def won():
+def won(player):
+    vals = X_VALS if player == 1 else O_VALS
     for triplet in WINNING_POSITIONS:
-        if triplet[0] in X_VALS and\
-           triplet[1] in X_VALS and\
-           triplet[2] in X_VALS:
-            return True
-    return False
+        if triplet[0] in vals and\
+           triplet[1] in vals and\
+           triplet[2] in vals:
+            return player
+    return None
 
 
 def main():
+    player = 1
+
+    show()
     while True:
-        show()
-        move()
-        if won():
+        res = move(player)
+        if not res:
             show()
+            continue
+        player = 1 if player == 2 else 2
+        res = won(player)
+        show()
+        if res:
+            print 'Celebrate player ' + res + '!'
             break
-    print 'Celebrate!'
 
 if __name__ == '__main__':
     main()
